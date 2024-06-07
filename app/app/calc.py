670e3841bd104a2d60f882ff8a7fe299813571cb
Wiktor Twardowski <wiktortwardowski1212@gmail.com>
2024-06-07 15:40:53 +0200

    database created

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y retrun result"""
+    return x + y
+
+
+def substract(x, y):
+    """Substract x from y and return result"""
+    return y - x
