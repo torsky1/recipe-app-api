7c12e2620e59c4c9ff8a9b5bed16bef3a7d9febb
Wiktor Twardowski <wiktortwardowski1212@gmail.com>
2024-06-07 15:54:46 +0200

    database created linting fixedv3

--- app/app/tests.py
+++ app/app/tests.py
@@ -12,8 +12,8 @@ class CalacTests(SimpleTestCase):
         """Test adding numbers together"""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
     def test_subtract_numbers(self):
         """Test substracting numbers"""
         res = calc.substract(10, 15)
         self.assertEqual(res, 5)
-        
